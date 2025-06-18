139ce91d58c1a02080cf6a76b281a078ad822328
brauliocadenaespinoza@gmail.com <brauliocadenaespinoza@gmail.com>
2025-06-18 17:43:08 -0600

    Configurar entorno y angula otra vez8

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
-import { GalleryComponent } from '../../components/gallery/gallery.component';
+import { Component } from '@angular/core';
+import { PhotoCollageComponent } from '../../components/photo-collage/photo-collage.component';
+
 @Component({
   selector: 'app-home',
-  imports: [GalleryComponent, GalleryComponent],
+  standalone: true,
+  imports: [PhotoCollageComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-    @ViewChild(GalleryComponent) galleryComponent!: GalleryComponent;
-
-  refrescarGaleria(): void {
-    this.galleryComponent.ngOnInit(); // Fuerza la recarga de fotos
-  }
-
-}
+export class HomeComponent {}
