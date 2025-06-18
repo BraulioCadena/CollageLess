32d38e08afc76f1295ce8e92e598827e5727af25
brauliocadenaespinoza@gmail.com <brauliocadenaespinoza@gmail.com>
2025-06-18 16:54:58 -0600

    Configurar entorno y angula otra vez8

--- src/app/components/photo-collage/photo-collage.component.ts
+++ src/app/components/photo-collage/photo-collage.component.ts
@@ -2,36 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoService } from '../../services/photo.service';
 
+interface Photo {
+  id: number;
+  filename: string;
+  url: string;
+}
+
 @Component({
   selector: 'app-photo-collage',
-  standalone: true, 
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './photo-collage.component.html',
   styleUrls: ['./photo-collage.component.scss']
 })
 export class PhotoCollageComponent implements OnInit {
-  photos: any[] = [];
+  photos: Photo[] = [];
+  selectedFile!: File;
 
-  constructor(private photoService: PhotoService) {}
+  constructor(private photoService: PhotoService) { }
 
   ngOnInit(): void {
     this.loadPhotos();
   }
 
-  loadPhotos() {
+  loadPhotos(): void {
     this.photoService.getAllPhotos().subscribe((res) => {
       this.photos = res;
     });
   }
 
-  deletePhoto(id: number) {
+  onFileSelected(event: any): void {
+    this.selectedFile = event.target.files[0];
+  }
+  upload(): void {
+    if (this.selectedFile) {
+      this.photoService.upload(this.selectedFile).subscribe((photo) => {
+        this.photos.push(photo);
+        this.selectedFile = undefined!;
+      });
+    }
+  }
+
+
+  deletePhoto(id: number): void {
     this.photoService.deletePhoto(id).subscribe(() => {
       this.photos = this.photos.filter(p => p.id !== id);
     });
-  }
-
-  // 👇 Agrega esto si necesitas construir la URL de las fotos
-  photoUrl(filename: string): string {
-    return `http://localhost:8080/api/photos/view/${filename}`;
   }
 }
