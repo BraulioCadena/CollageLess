ad6310b48eed04059cc35a857d8218645138fec5
brauliocadenaespinoza@gmail.com <brauliocadenaespinoza@gmail.com>
2025-06-19 00:16:23 -0600

    Configurar entorno y angula otra vez8

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,21 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+
+export interface Photo {
+  id: number;
+  filename: string;
+  url: string;
+}
+
 @Injectable({ providedIn: 'root' })
 export class PhotoService {
   private readonly API = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
+  // Obtener todas las fotos desde el backend (Cloudinary)
   getAllPhotos() {
-    return this.http.get<any[]>(this.API);
-  }
-   upload(file: File) {
-    const formData = new FormData();
-    formData.append('file', file);
-    return this.http.post<any>(`${this.API}/upload`, formData);
+    return this.http.get<Photo[]>(this.API);
   }
 
+  // Subir una nueva imagen a Cloudinary
+  upload(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<Photo>(`${this.API}/upload`, formData);
+  }
+
+  // Eliminar una foto por ID
   deletePhoto(id: number) {
     return this.http.delete(`${this.API}/${id}`);
   }
