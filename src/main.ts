f584febb3a8eef89e33ed43de197a75182d49f1e
brauliocadenaespinoza@gmail.com <brauliocadenaespinoza@gmail.com>
2025-06-26 13:04:39 -0600

    Configurar ruta persistente34

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { HomeComponent } from './app/pages/home/home.component';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // 👈 necesario
+import { provideHttpClient } from '@angular/common/http'; 
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter([
       { path: '', component: HomeComponent }
     ]),
-    provideHttpClient() // 👈 esto soluciona el error de inyección
+    provideHttpClient() 
   ]
 });
